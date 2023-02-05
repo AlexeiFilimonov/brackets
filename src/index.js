aecfb18d69c47e4474493e1ef2ef7ff73dbe3334
alexeifilimonov <nlifeaf@mail.ru>
2023-02-06 01:02:54 +0300

    feat: add brackets check logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let prevLength = str.length;
+  bracketsConfig.forEach(element => {
+    str = str.replace(element[0]+element[1],'')
+  }); 
+  while (str.length!=prevLength) {
+    prevLength = str.length;
+    bracketsConfig.forEach(element => {
+      str = str.replace(element[0]+element[1],'')
+    }); 
+  }
+  if (str.length === 0) return true;
+  return false;
 }
